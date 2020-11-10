ef96446b97846dba06d9e5de1605d8c8e95e934d
Pontus Johansson <pontusjohansson8@outlook.com>
2020-11-10 12:09:35 +0100

    PressurePlate WIP

---
+++ TangledTogether/Assets/Scripts/LevelMechanics/Objects/PressurePlate.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject moveObject;
+    public float moveDist;
+    public float speedDown;
+    public float speedUp;
+    public float weightNeeded;
+    private Vector3 startPos;
+    private Vector3 newPos;
+    [SerializeField] private float currentWeight;
+    [SerializeField] private bool activate;
+	public float test;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startPos = moveObject.transform.position;
+        newPos = new Vector3(startPos.x, startPos.y + moveDist, startPos.z);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+		if (currentWeight >= weightNeeded)
+		{
+			moveObject.transform.position = Vector3.MoveTowards(moveObject.transform.position, newPos, speedDown * Time.deltaTime);
+			if (moveObject.transform.position == newPos)
+			{
+				activate = true;
+			}
+		}
+		else
+		{
+			moveObject.transform.position = Vector3.MoveTowards(moveObject.transform.position, startPos, speedUp * Time.deltaTime);
+			activate = false;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.relativeVelocity.y <= test)
+			currentWeight += collision.rigidbody.mass;
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		//TODO: Continue here
+		currentWeight -= collision.rigidbody.mass;
+	}
+}
---
+++ TangledTogether/Assets/Scripts/PlayerAbilities/Yeet.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yeet : MonoBehaviour
+{
+	private PlayerInput playerInput;
+	private GameObject otherPlayer;
+
+	public float viewRadius;
+	[Range(0, 360)]
+	public float viewAngle;
+
+	public LayerMask targetMask;
+	public LayerMask obstacleMask;
+
+	[Header("Throw")]
+	public float h = 25;
+	public float gravity = -18;
+	public float angle = 1;
+	private Vector3 launchDirection;
+	public float throwForce;
+
+	private void Awake()
+	{
+		playerInput = gameObject.GetComponent<PlayerInputHolder>().playerInput;
+	}
+
+	void Update()
+	{
+		YeetOtherPlayer();
+	}
+
+	void YeetOtherPlayer()
+	{
+		if (Input.GetKey(playerInput.yeet) && !playerInput.disableThrow)
+		{
+			Debug.Log("Prepare to yeet");
+			playerInput.disableMovement = true;
+			CheckOtherPlayerPos();
+
+			Debug.DrawRay(gameObject.transform.position, GetDirection(), Color.cyan);
+		}
+		else if (Input.GetKeyUp(playerInput.yeet))
+		{
+			Debug.Log("Yeet denied");
+			playerInput.disableMovement = false;
+		}
+	}
+
+	void CheckOtherPlayerPos()
+	{
+		Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+		if(targetsInViewRadius.Length != 0)
+		{
+			otherPlayer = targetsInViewRadius[0].gameObject;
+			Transform target = otherPlayer.transform;
+			Vector3 dirToTarget = (target.position - transform.position).normalized;
+			if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+			{
+				float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+				if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+				{
+					Debug.Log("Y E E T");
+					Launch();
+					playerInput.disableMovement = false;
+				}
+			}
+		}
+	}
+
+	void Launch()
+	{
+		launchDirection = GetDirection();
+		otherPlayer.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+		otherPlayer.GetComponent<Rigidbody>().AddForce(launchDirection * throwForce, ForceMode.VelocityChange);
+		playerInput.disableThrow = true;
+		otherPlayer.GetComponent<PlayerInputHolder>().playerInput.disableThrow = true;
+		StartCoroutine(WaitForThrow());
+	}
+
+	Vector3 GetDirection()
+	{
+		Vector3 direction = -gameObject.transform.forward + Vector3.up * angle;
+		direction.Normalize();
+
+		return direction;
+	}
+
+	IEnumerator WaitForThrow()
+	{
+		yield return new WaitForSeconds(1);
+		playerInput.disableThrow = false;
+		otherPlayer.GetComponent<PlayerInputHolder>().playerInput.disableThrow = false;
+	}
+}
